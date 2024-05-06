3439fbfc773f449980e26d1cc05d56b12d1f620d
fares <fares.haliim@gmail.com>
2024-05-06 11:18:42 +0300

    start the project

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// @ts-ignore
+import eslint from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint],
+});
