1b52f1154de5b63fe316fba9d8c50da41dd9abf8
Fares Ahmed <fares.haliim@gmail.com>
2024-06-13 00:41:09 +0300

    finish up the project ✨

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import React from "react";
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
